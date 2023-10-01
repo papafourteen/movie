d4b12984613fd3f9b47815503cf5ea7e30885fa5
Torma Andris <andris.for@hotmail.com>
2023-10-01 17:55:32 +0200

    second build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode})=>{
+  const env=loadEnv(mode,process.cwd())
   
+  
+  
+  return{
+
+  
+    base:'/movie',
   plugins: [react()],
+  build:{
+  chunkSizeWarningLimit:1600
+  },
+  server:{
+    port:env.VITE_PORT
+  }
+}
 })
